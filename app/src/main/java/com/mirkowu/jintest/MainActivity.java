8e3e591afa28a6fa483aa12c6464ddced9929924
MirkoWu <709651717@qq.com>
2020-12-03 22:53:12 +0800

    update 实践了cpu类型混用

--- app/src/main/java/com/mirkowu/jintest/MainActivity.java
+++ app/src/main/java/com/mirkowu/jintest/MainActivity.java
@@ -1,12 +1,19 @@
 package com.mirkowu.jintest;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.mirkowu.jintest.jni.JniDynamicRegisterTest;
 import com.mirkowu.jintest.jni.JniTest;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +31,28 @@ public class MainActivity extends AppCompatActivity {
         tv2.setText(JniTest.stringFromJniWithStr(this,"我是通过静态注册返回的Java"));
         tv3.setText(JniDynamicRegisterTest.a( ));
         tv4.setText(JniDynamicRegisterTest.b(this,"我是通过动态注册返回的"));
+
+
+        Log.d("MainActivity","SUPPORTED_ABIS = "+ Arrays.toString(Build.SUPPORTED_ABIS));
+        Log.d("MainActivity","SUPPORTED_64_BIT_ABIS = "+ Arrays.toString(Build.SUPPORTED_64_BIT_ABIS));
+        Log.d("MainActivity","SUPPORTED_32_BIT_ABIS = "+ Arrays.toString(Build.SUPPORTED_32_BIT_ABIS));
+        Log.d("MainActivity","abi[0] = "+Build.SUPPORTED_ABIS[0]);
+        Log.d("MainActivity","CPU_ABI = "+Build.CPU_ABI);
+        Log.d("MainActivity","nativeLibraryDir = "+getApplication().getApplicationInfo().nativeLibraryDir);
+
+//        this.getApplication().getApplicationInfo().nativeLibraryDir;
+
+        //32位和64位 的so文件不能混用 ，但是32位之间可以混用（但不建议，最好使用对应的类型），64位之间可以混用（但不建议，最好使用对应的类型）
+        //abiFilters 最好配置，如果支持64位，且文件夹里也有对应so文件，可以配上对应cpu类型，例arm64-v8a；如果没有64位so文件，不要配这个类型的
+
+
+
+        tv4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
 
